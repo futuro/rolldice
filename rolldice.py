c25895cf02005e3a17de23f283f9e24405398325
Mark Merritt <mark@marktmerritt.com>
2011-05-19 14:33:20 -0400

    let's actually get this thing onto github

---
+++ rolldice.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+
+# run it like:
+# rolldice 2d20
+# WARNING: bad input is not handled
+
+from random import randint
+from sys import argv
+
+if len(argv) < 2:
+	print("must have an argument, ie 2d20")
+	exit(1)
+args = [int(i) for i in argv[1].split('d')]
+print(sum(randint(1,args[1]+1) for i in range(args[0])))
+
